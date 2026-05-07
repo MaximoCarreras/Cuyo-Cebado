da521a65539d46fe75e0dc41b44568d272da8c33
MaximoCarreras <maxcarreras@users.noreply.github.com>
2026-05-07 20:55:42 -0300

    SSSS

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -25,40 +25,48 @@ export default function Navbar() {
 
   return (
     <>
-      {/* Barra que fluye (se oculta al bajar) */}
       <div className="announcement-bar">
         <p><b>Beneficio Cuyo:</b> Entrega personalizada en Mendoza y San Luis. Envíos protegidos a todo el país.</p>
       </div>
 
-      <nav className="navbar" id="navbar">
-        <div className="navbar__left">
-          <Link to="/" className="navbar__logo" onClick={handleNavClick}>
-            <img src="/logo.png" alt="Cuyo Cebado" className="navbar__logo-img" />
-          </Link>
-        </div>
+      <nav className="navbar">
+        <div className="navbar__container">
 
-        <div className={`navbar__center ${isMobileOpen ? 'navbar__center--open' : ''}`}>
-          <ul className="navbar__links">
-            {navLinks.map(link => (
-              <li key={link.to}>
-                <Link to={link.to} onClick={handleNavClick}>{link.label}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+          {/* IZQUIERDA: Logo */}
+          <div className="navbar__left">
+            <Link to="/" className="navbar__logo" onClick={handleNavClick}>
+              <img src="/logo.png" alt="Cuyo Cebado" className="navbar__logo-img" />
+            </Link>
+          </div>
 
-        <div className="navbar__right">
-          <Link to="/carrito" className="navbar__cart-link" onClick={handleNavClick}>
-            <span className="material-symbols-outlined">shopping_cart</span>
-            {cartCount > 0 && <span className="cart-badge">{cartCount}</span>}
-          </Link>
+          {/* CENTRO: Links (Se ocultan en móvil, se ven en escritorio) */}
+          <div className={`navbar__center ${isMobileOpen ? 'navbar__center--open' : ''}`}>
+            <ul className="navbar__links">
+              {navLinks.map(link => (
+                <li key={link.to}>
+                  <Link to={link.to} onClick={handleNavClick}>{link.label}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-          <button
-            className={`navbar__toggle ${isMobileOpen ? 'navbar__toggle--active' : ''}`}
-            onClick={() => setIsMobileOpen(!isMobileOpen)}
-          >
-            <span className="material-symbols-outlined">{isMobileOpen ? 'close' : 'menu'}</span>
-          </button>
+          {/* DERECHA: Carrito y Hamburguesa */}
+          <div className="navbar__right">
+            <Link to="/carrito" className="navbar__cart-link" onClick={handleNavClick}>
+              <span className="material-symbols-outlined">shopping_cart</span>
+              {cartCount > 0 && <span className="cart-badge">{cartCount}</span>}
+            </Link>
+
+            <button
+              className={`navbar__toggle ${isMobileOpen ? 'navbar__toggle--active' : ''}`}
+              onClick={() => setIsMobileOpen(!isMobileOpen)}
+            >
+              <span className="material-symbols-outlined">
+                {isMobileOpen ? 'close' : 'menu'}
+              </span>
+            </button>
+          </div>
+
         </div>
       </nav>
     </>
