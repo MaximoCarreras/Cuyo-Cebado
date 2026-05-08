70a14e198a228ac95d6eb221e07aa72e9681ebbd
MaximoCarreras <maxcarreras@users.noreply.github.com>
2026-05-08 16:25:11 -0300

    ss

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,40 +13,46 @@ export default function Navbar() {
 
   return (
     <nav className="navbar">
-      {/* 1. IZQUIERDA: LOGO */}
-      <div className="navbar__left">
-        <Link to="/" className="navbar__brand" onClick={closeMenu}>
-          <img src="/logo.png" alt="Cuyo Cebado" className="navbar__logo" />
-          <span className="navbar__brand-text">CUYO CEBADO</span>
-        </Link>
+      <div className="navbar__container">
+
+        {/* BLOQUE IZQUIERDA: LOGO */}
+        <div className="navbar__column navbar__column--left">
+          <Link to="/" className="navbar__brand" onClick={closeMenu}>
+            <img src="/logo.png" alt="Cuyo Cebado" className="navbar__logo" />
+            <span className="navbar__brand-text">CUYO CEBADO</span>
+          </Link>
+        </div>
+
+        {/* BLOQUE CENTRO: LINKS (Solo Desktop) */}
+        <div className="navbar__column navbar__column--center">
+          <ul className="navbar__desktop-menu">
+            <li><Link to="/" className="navbar__link">Inicio</Link></li>
+            <li><Link to="/productos" className="navbar__link">Productos</Link></li>
+            <li><Link to="/nosotros" className="navbar__link">Nosotros</Link></li>
+            <li><Link to="/guia-curado" className="navbar__link">Guía de Curado</Link></li>
+          </ul>
+        </div>
+
+        {/* BLOQUE DERECHA: ICONOS */}
+        <div className="navbar__column navbar__column--right">
+          <Link to="/carrito" className="navbar__cart-container" onClick={closeMenu}>
+            <span className="material-symbols-outlined cart-icon-main">shopping_cart</span>
+            {totalItems > 0 && <div className="cart-badge-premium">{totalItems}</div>}
+          </Link>
+
+          <button className="navbar__hamburger" onClick={toggleMenu}>
+            <span className="material-symbols-outlined">{isMenuOpen ? 'close' : 'menu'}</span>
+          </button>
+        </div>
+
       </div>
 
-      {/* 2. CENTRO: LINKS (Escritorio) */}
-      <ul className={`navbar__menu ${isMenuOpen ? 'navbar__menu--open' : ''}`}>
-        <li><Link to="/" className="navbar__link" onClick={closeMenu}>Inicio</Link></li>
-        <li><Link to="/productos" className="navbar__link" onClick={closeMenu}>Productos</Link></li>
-        <li><Link to="/nosotros" className="navbar__link" onClick={closeMenu}>Nosotros</Link></li>
-        <li><Link to="/guia-curado" className="navbar__link" onClick={closeMenu}>Guía de Curado</Link></li>
-      </ul>
-
-      {/* 3. DERECHA: CARRITO Y LUEGO HAMBURGUESA */}
-      <div className="navbar__right">
-        {/* El contenedor del carrito tiene que ser relativo para que el número no flote */}
-        <Link to="/carrito" className="navbar__cart-container" onClick={closeMenu}>
-          <span className="material-symbols-outlined cart-icon-main">
-            shopping_cart
-          </span>
-          {totalItems > 0 && (
-            <div className="cart-badge-premium">{totalItems}</div>
-          )}
-        </Link>
-
-        {/* La hamburguesa queda a la derecha del todo en móvil */}
-        <button className="navbar__hamburger" onClick={toggleMenu} aria-label="Menu">
-          <span className="material-symbols-outlined">
-            {isMenuOpen ? 'close' : 'menu'}
-          </span>
-        </button>
+      {/* MENÚ MÓVIL (DRAWER) */}
+      <div className={`navbar__mobile-drawer ${isMenuOpen ? 'open' : ''}`}>
+        <Link to="/" onClick={closeMenu}>Inicio</Link>
+        <Link to="/productos" onClick={closeMenu}>Productos</Link>
+        <Link to="/nosotros" onClick={closeMenu}>Nosotros</Link>
+        <Link to="/guia-curado" onClick={closeMenu}>Guía de Curado</Link>
       </div>
 
       {isMenuOpen && <div className="navbar__overlay" onClick={closeMenu}></div>}
