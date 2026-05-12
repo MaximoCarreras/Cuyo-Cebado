aa40d72c2ad9f43c246c7f98cb9b9e66933e00f8
MaximoCarreras <maxcarreras@users.noreply.github.com>
2026-05-11 22:50:30 -0300

    s

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,61 +1,119 @@
 import { useState } from 'react';
+
 import { Link } from 'react-router-dom';
+
 import { useCart } from '../../context/CartContext';
-import LogoCuyo from './LogoCuyo'; // Importamos el logo en código
+
 import './Navbar.css';
 
+
+
 export default function Navbar() {
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const { cart } = useCart();
+
   const totalItems = cart.reduce((acc, item) => acc + item.quantity, 0);
 
+
+
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   const closeMenu = () => setIsMenuOpen(false);
 
+
+
   return (
+
     <nav className="navbar">
+
       <div className="navbar__container">
 
-        {/* IZQUIERDA: LOGO EN CÓDIGO (SVG) */}
+
+
+        {/* IZQUIERDA: LOGO */}
+
         <div className="navbar__left">
+
           <Link to="/" className="navbar__brand" onClick={closeMenu}>
-            <LogoCuyo className="navbar__logo-svg" />
+
+            <img src="/logo.png" alt="Cuyo Cebado" className="navbar__logo" />
+
             <span className="navbar__brand-text">CUYO CEBADO</span>
+
           </Link>
+
         </div>
 
-        {/* CENTRO: LINKS */}
+
+
+        {/* CENTRO: LINKS (Solo se ven en escritorio) */}
+
         <ul className="navbar__desktop-menu">
+
           <li><Link to="/" className="navbar__link">Inicio</Link></li>
+
           <li><Link to="/productos" className="navbar__link">Productos</Link></li>
+
           <li><Link to="/nosotros" className="navbar__link">Nosotros</Link></li>
+
           <li><Link to="/guia-curado" className="navbar__link">Guía de Curado</Link></li>
+
         </ul>
 
+
+
         {/* DERECHA: CARRITO + HAMBURGUESA */}
+
         <div className="navbar__right">
+
           <Link to="/carrito" className="navbar__cart-container" onClick={closeMenu}>
+
             <span className="material-symbols-outlined cart-icon-main">shopping_cart</span>
+
             {totalItems > 0 && <div className="cart-badge-premium">{totalItems}</div>}
+
           </Link>
 
+
+
           <button className="navbar__hamburger" onClick={toggleMenu} aria-label="Menu">
+
             <span className="material-symbols-outlined">
+
               {isMenuOpen ? 'close' : 'menu'}
+
             </span>
+
           </button>
+
         </div>
+
       </div>
 
-      {/* MENÚ MÓVIL */}
+
+
+      {/* MENÚ MÓVIL (DRAWER) - Completamente separado */}
+
       <div className={`navbar__mobile-drawer ${isMenuOpen ? 'open' : ''}`}>
+
         <Link to="/" onClick={closeMenu}>Inicio</Link>
+
         <Link to="/productos" onClick={closeMenu}>Productos</Link>
+
         <Link to="/nosotros" onClick={closeMenu}>Nosotros</Link>
+
         <Link to="/guia-curado" onClick={closeMenu}>Guía de Curado</Link>
+
       </div>
+
+
 
       {isMenuOpen && <div className="navbar__overlay" onClick={closeMenu}></div>}
+
     </nav>
+
   );
+
 }
