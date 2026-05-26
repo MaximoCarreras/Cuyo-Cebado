e42f65e925c89f3453cc4b14940e34a093d6ff24
MaximoCarreras <maxcarreras@users.noreply.github.com>
2026-05-25 22:09:51 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import ProductsPage from './pages/ProductsPage/ProductsPage';
 import CategoryPage from './pages/CategoryPage/CategoryPage';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import AuthPage from './pages/Auth/AuthPage';
-// RUTA CORREGIDA: Apunta a tu nueva carpeta Dashboard
+// Ruta corregida a la subcarpeta Dashboard
 import AdminDashboard from './pages/Admin/Dashboard/AdminDashboard';
 import SuccessPage from './pages/Checkout/SuccessPage';
 import FailurePage from './pages/Checkout/FailurePage';
@@ -36,8 +36,6 @@ export default function App() {
           <Route path="/admin" element={<AdminDashboard />} />
           <Route path="/pago-exitoso" element={<SuccessPage />} />
           <Route path="/pago-fallido" element={<FailurePage />} />
-          
-          {/* RUTA 404: Siempre tiene que ir al final de la lista */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
